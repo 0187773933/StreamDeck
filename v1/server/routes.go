86329a760f44279e61640e4b08f796cd89826b86
0187773933 <collincerbus@student.olympic.edu>
2024-06-12 16:59:44 -0400

    47

--- v1/server/routes.go
+++ v1/server/routes.go
@@ -1,7 +1,8 @@
 package server
 
 import (
-	// "fmt"
+	"fmt"
+	"strings"
 	// "time"
 	filepath "path/filepath"
 	fiber "github.com/gofiber/fiber/v2"
@@ -10,24 +11,6 @@ import (
 	bolt_api "github.com/boltdb/bolt"
 	ui_wrapper "github.com/0187773933/StreamDeck/v1/ui"
 )
-
-// var ui_html_pages = map[ string ]string {
-// 	"/": "./v1/server/html/admin.html" ,
-// 	"/users": "./v1/server/html/admin_view_users.html" ,
-// 	// "/user/new": "./v1/server/html/admin_user_new.html" ,
-// 	"/user/new/handoff/:uuid": "./v1/server/html/admin_user_new_handoff.html" ,
-// 	"/user/checkin": "./v1/server/html/admin_user_checkin.html" ,
-// 	"/user/checkin/:uuid": "./v1/server/html/admin_user_checkin.html" ,
-// 	"/user/checkin/:uuid/edit": "./v1/server/html/admin_user_checkin.html" ,
-// 	"/user/checkin/new": "./v1/server/html/admin_user_checkin.html" ,
-// 	"/user/edit/:uuid": "./v1/server/html/admin_user_edit.html" ,
-// 	"/checkins": "./v1/server/html/admin_view_total_checkins.html" ,
-// 	"/emails": "./v1/server/html/admin_view_all_emails.html" ,
-// 	"/phone-numbers": "./v1/server/html/admin_view_all_phone_numbers.html" ,
-// 	"/barcodes": "./v1/server/html/admin_view_all_barcodes.html" ,
-// 	"/sms": "./v1/server/html/admin_sms_all_users.html" ,
-// 	"/email": "./v1/server/html/admin_email_all_users.html" ,
-// }
 
 func ( s *Server ) PressButton( context *fiber.Ctx ) ( error ) {
 	if validate_admin( context ) == false { return serve_failed_attempt( context ) }
@@ -164,24 +147,20 @@ func ( s *Server ) RenderPage( context *fiber.Ctx ) ( error ) {
 }
 
 func ( s *Server ) GetPageAddTiledImage( context *fiber.Ctx ) ( error ) {
-	if validate_admin( context ) == false { return serve_failed_attempt( context ) }
 	htmlContent := `
 	<!DOCTYPE html>
 	<html>
 	<head>
-	    <title>Upload Image</title>
+		<title>Upload Image</title>
 	</head>
 	<body>
-	    <form action="/page/add/tiled" method="post" enctype="multipart/form-data">
-	        <input type="file" name="image" required /><br><br>
-	        <input type="text" name="MP3String" placeholder="MP3 String" /><br><br>
-	        <input type="text" name="SingleClickString" placeholder="Single Click Command" /><br><br>
-	        <input type="text" name="ReturnPageString" placeholder="Return Page" /><br><br>
-	        <input type="text" name="XSize" placeholder="3" /><br><br>
-	        <input type="text" name="YSize" placeholder="2" /><br><br>
-	        <input type="text" name="IconSize" placeholder="72" /><br><br>
-	        <input type="submit" value="Upload" />
-	    </form>
+		<form action="/page/add/tiled" method="post" enctype="multipart/form-data">
+			<input type="file" name="image" required /><br><br>
+			<input type="text" name="MP3String" placeholder="MP3 String" /><br><br>
+			<input type="text" name="SingleClickString" placeholder="Single Click Command" /><br><br>
+			<input type="text" name="ReturnPageString" placeholder="Return Page" /><br><br>
+			<input type="submit" value="Upload" />
+		</form>
 	</body>
 	</html>
 	`
@@ -220,20 +199,144 @@ func ( s *Server ) PageAddTiledImage( context *fiber.Ctx ) ( error ) {
 		ReturnPage:  context.FormValue("ReturnPageString"),
 	}
 
-	x_size := context.FormValue("XSize")
-	y_size := context.FormValue("YSize")
-	icon_size := context.FormValue("IconSize")
-	x_size_int , _ := strconv.Atoi( x_size )
-	y_size_int , _ := strconv.Atoi( y_size )
-	icon_size_int , _ := strconv.Atoi( icon_size )
-
-	// page_id := s.UI.AddImageAsTiledButton(targetPath, button)
-	page_id := s.UI.AddImageAsTiledButtonCustom( targetPath, button , x_size_int , y_size_int , icon_size_int )
+	page_id := s.UI.AddImageAsTiledButton(targetPath, button)
 
 	return context.JSON( fiber.Map{
 		"route": "/page/add/tiled" ,
 		"page_id": page_id ,
 		"result": "success" ,
+	})
+}
+
+
+func ( s *Server ) GetButtonAdd( context *fiber.Ctx ) ( error ) {
+	html_content := `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Add Button</title>
+		<script type="text/javascript">
+			document.addEventListener("DOMContentLoaded", function() {
+				var counter = 1;
+				document.getElementById("addOption").addEventListener("click", function() {
+					var container = document.getElementById("optionsContainer");
+					var inputKey = document.createElement("input");
+					inputKey.type = "text";
+					inputKey.name = "OptionKey-" + counter;
+					inputKey.placeholder = "Option Key";
+					var inputValue = document.createElement("input");
+					inputValue.type = "text";
+					inputValue.name = "OptionValue-" + counter;
+					inputValue.placeholder = "Option Value";
+					container.appendChild(inputKey);
+					container.appendChild(inputValue);
+					container.appendChild(document.createElement("br"));
+					counter++;
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<form action="/button/add" method="post" enctype="multipart/form-data">
+			<input type="file" name="image" /><br><br>
+			<input type="text" name="ID" placeholder="Button-ID" /><br><br>
+			<input type="text" name="MP3String" placeholder="MP3 String" /><br><br>
+			<input type="text" name="SingleClickString" placeholder="Single Click Command" /><br><br>
+			<input type="text" name="DoubleClickString" placeholder="Double Click Command" /><br><br>
+			<input type="text" name="TripleClickString" placeholder="Triple Click Command" /><br><br>
+			<input type="text" name="ReturnPageString" placeholder="Return Page" /><br><br>
+			<div id="optionsContainer"></div>
+			<button type="button" id="addOption">Add Option</button><br><br>
+			<input type="submit" value="Upload" />
+		</form>
+	</body>
+	</html>
+	`
+	context.Set( "Content-Type" , "text/html" )
+	return context.SendString( html_content )
+}
+
+func (s *Server) GetButtonEdit(context *fiber.Ctx) error {
+    button_id := context.Params( "id" )
+    fmt.Println( "trying to edit" , button_id )
+    _ , exists := s.UI.Buttons[ button_id ]
+    if exists == false {
+        return context.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Button not found"})
+    }
+    button := s.UI.Buttons[ button_id ]
+    // Generate the HTML form with button data filled in
+    htmlContent := fmt.Sprintf(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Edit Button</title>
+    </head>
+    <body>
+        <form action="/button/%s/edit" method="post" enctype="multipart/form-data">
+            <input type="file" name="image" /><br><br> <!-- Allow image update, no 'required' if image already exists -->
+            <input type="text" name="ID" value="%s" readonly /><br><br> <!-- ID is readonly to prevent changes -->
+            <input type="text" name="MP3String" value="%s" placeholder="MP3 String" /><br><br>
+            <input type="text" name="SingleClickString" value="%s" placeholder="Single Click Command" /><br><br>
+            <input type="text" name="DoubleClickString" value="%s" placeholder="Double Click Command" /><br><br>
+            <input type="text" name="TripleClickString" value="%s" placeholder="Triple Click Command" /><br><br>
+            <input type="text" name="ReturnPageString" value="%s" placeholder="Return Page" /><br><br>
+            <input type="submit" value="Update" />
+        </form>
+    </body>
+    </html>
+    `, button_id, button_id, button.MP3, button.SingleClick, button.DoubleClick, button.TripleClick, button.ReturnPage)
+
+    context.Set("Content-Type", "text/html")
+    return context.SendString(htmlContent)
+}
+
+func ( s *Server ) ButtonAdd( context *fiber.Ctx ) ( error ) {
+	if validate_admin( context ) == false { return serve_failed_attempt( context ) }
+	form , err := context.MultipartForm()
+	if err != nil { return err }
+	files := form.File[ "image" ]
+	var image_path string
+	if len( files ) == 0 {
+		// use default icon image if not sent
+		image_path = filepath.Join( "./images" , "1f49a.png" )
+	} else {
+		file := files[ 0 ]
+		filename := filepath.Base( file.Filename )
+		image_path = filepath.Join( "./images" , filename )
+		err = context.SaveFile( file , image_path );
+		if err != nil { return err }
+	}
+
+	button_id := context.FormValue( "ID" )
+    options := make(map[string]string)
+    for key, values := range form.Value {
+        if strings.HasPrefix( key , "OptionKey-" ) {
+            optionIndex := strings.TrimPrefix( key , "OptionKey-" )
+            valueKey := "OptionValue-" + optionIndex
+            if value, ok := form.Value[valueKey]; ok && len(value) > 0 {
+                options[values[0]] = value[0]
+            }
+        }
+    }
+
+	button := ui_wrapper.Button{
+		Id: button_id ,
+		MP3: context.FormValue( "MP3String" ) ,
+		Image: image_path ,
+		SingleClick: context.FormValue( "SingleClickString" ) ,
+		DoubleClick: context.FormValue( "DoubleClickString" ) ,
+		TripleClick: context.FormValue( "TripleClickString" ) ,
+		ReturnPage: context.FormValue( "ReturnPageString" ) ,
+		Options: options ,
+	}
+
+	s.UI.AddButton( button_id , button )
+
+	return context.JSON( fiber.Map{
+		"route": "/button/add" ,
+		"result": "success" ,
+		"id": button_id ,
+		"button": button ,
 	})
 }
 
@@ -252,10 +355,6 @@ func ( s *Server ) SetupRoutes() {
 		return ctx.SendString( "<h1>Stream Deck Server</h1>" )
 	})
 
-	s.FiberApp.Get( "/login" , s.ServeLoginPage )
-	s.FiberApp.Post( "/login" , s.HandleLogin )
-	s.FiberApp.Get( "/logout" , s.Logout )
-
 	s.FiberApp.Get( "/show" , s.Show )
 	s.FiberApp.Get( "/brightness/increase" , s.IncreaseBrightness )
 	s.FiberApp.Get( "/brightness/decrease" , s.DecreaseBrightness )
@@ -265,8 +364,11 @@ func ( s *Server ) SetupRoutes() {
 	s.FiberApp.Get( "/sleep" , s.Sleep )
 	s.FiberApp.Get( "/wake" , s.Wake )
 	s.FiberApp.Get( "/:button" , s.PressButton )
+	s.FiberApp.Get( "/button/:id/edit" , s.GetButtonEdit )
+	s.FiberApp.Get( "/button/add" , s.GetButtonAdd )
 	s.FiberApp.Post( "/button/add" , s.ButtonAdd )
 	s.FiberApp.Get( "/page/:id" , s.RenderPage )
 	s.FiberApp.Get( "/page/add/tiled" , s.GetPageAddTiledImage )
 	s.FiberApp.Post( "/page/add/tiled" , s.PageAddTiledImage )
+
 }
